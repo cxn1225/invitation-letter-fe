40746ff7a87ef33bbb5da1e249d28f972b6d68cf
xiaoning <xiaoning.chi@huixiaoer.com>
2026-07-27 15:09:17 +0800

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,20 +30,7 @@ export const router = createRouter({
   },
 });
 
-router.beforeEach((to, from, next) => {
-  // 如果是登录页或者404等其他白名单页面，直接放行
-  if (whiteRoutes.includes(to.path)) {
-    return next();
-  }
 
-  const userStore = useUserStore();
-  if (!userStore.accessToken) {
-    return next({ path: "/login", query: { redirect: to.fullPath } });
-  }
-
-  // 上述条件都不满足，放行
-  return next();
-});
 router.afterEach((to: RouteLocationNormalized) => {
   document.title = to.meta.title || "";
 });
