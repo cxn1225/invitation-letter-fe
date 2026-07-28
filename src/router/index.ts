05b5a25b7ad4c1074734c4f129472f878b4f32c5
xiaoning <xiaoning.chi@huixiaoer.com>
2026-07-28 11:24:32 +0800

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,5 @@
 import type { RouteLocationNormalized, RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHistory } from "vue-router";
-import { useUserStore } from "@/store/modules/user";
-import { whiteRoutes } from "@/utils/config";
 
 const constance: RouteRecordRaw[] = [
   {
